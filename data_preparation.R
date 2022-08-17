cc2d49819a25f4cee60b4d9f632a7317c2d938f9
Agnieszka Choczyńska <agachocz@gmail.com>
2022-08-17 16:54:43 +0200

    Basic VAR model for DAX and ZEW Economic Growth

--- data_preparation.R
+++ data_preparation.R
@@ -1,9 +1,24 @@
 library(dplyr)
 library(ggplot2)
 library(tseries)
+library(vars)
 
-dax <- read.csv("data/dax_m.csv", sep =",", header=T, dec = ".")[,c(1,5)]
-dax$Date <- as.Date(dax$Date, format = "%Y-%m-%d")
+dax <- read.csv("data/dax_m.csv", sep =",", header=T, dec = ".")[,c(1,5)] %>% 
+  mutate(Date = as.Date(Date, format = "%Y-%m-%d")) %>%
+  mutate(Month = format(Date, "%m-%Y"))
 zew <- read.csv("data/ZEW_eurozone.csv", header = T, sep = ";", dec = ",")
 colnames(zew) <- c("Date", "EconomicGrowth", "Inflation", "CurrentSituation", "STOXX50", "InterestRate")
-zew$Date <- as.Date(zew$Date, format = "%d.%m.%Y")
+zew <- zew %>% mutate(Date = as.Date(Date, format = "%d.%m.%Y")) %>%
+  mutate(Month = format(Date, "%m-%Y"))
+
+data <- full_join(dax, zew, by = "Month") %>% dplyr::select(Month, Close, EconomicGrowth) %>%
+  mutate(Close = Close - lag(Close), EconomicGrowth = EconomicGrowth - lag(EconomicGrowth)) %>%
+  slice(-1) %>% filter(across(everything(), complete.cases))
+
+adf.test(data$Close) # stationary
+adf.test(data$EconomicGrowth) # stationary
+
+VARselect(data[,-1], lag.max = 15, type="const")
+model <- VAR(data[,-1], p = 1, type = "none")
+summary(model)
+coefficients(model)
