ea7b176f56fa88dcac2da933c2487a91ce4f31dd
Agnieszka Choczyńska <agachocz@gmail.com>
2023-04-05 15:26:19 +0200

    Converting currency from zł to EUR.

--- data_preparation.R
+++ data_preparation.R
@@ -6,29 +6,39 @@ library(vars)
 
 # read data
 dax_m <- read.csv("data/dax_m.csv", sep =",", header=T, dec = ".")[,c(1,5,6)] %>%
-  rename(Date = Data, Close = Zamkniecie, Volume = Wolumen) %>%
+  rename(Date = Data, DAX = Zamkniecie, Vol_GER = Wolumen) %>%
   mutate(Date = as.Date(Date, format = "%Y-%m-%d"))
 
 dax_d <- read.csv("data/dax_d.csv", sep =",", header=T, dec = ".")[,c(1,5,6)] %>%
-  rename(Date = Data, Close = Zamkniecie, Volume = Wolumen) %>%
+  rename(Date = Data, DAX = Zamkniecie, Vol_GER = Wolumen) %>%
   mutate(Date = as.Date(Date, format = "%Y-%m-%d"))
 
 wig_m <- read.csv("data/wig_m.csv", sep =",", header=T, dec = ".")[,c(1,5,6)] %>% 
-  rename(Date = Data, Close = Zamkniecie, Volume = Wolumen) %>%
+  rename(Date = Data, WIG = Zamkniecie, Vol_PL = Wolumen) %>%
   mutate(Date = as.Date(Date, format = "%Y-%m-%d"))
 
 wig_d <- read.csv("data/wig_d.csv", sep =",", header=T, dec = ".")[,c(1,5,6)] %>% 
-  rename(Date = Data, Close = Zamkniecie, Volume = Wolumen) %>%
+  rename(Date = Data, WIG = Zamkniecie, Vol_PL = Wolumen) %>%
   mutate(Date = as.Date(Date, format = "%Y-%m-%d"))
   #mutate(Month = format(Date, "%m-%Y"))
 
+plneur_m <- read.csv("data/plneur_m.csv", sep = ",")[,c(1,5)] %>%
+  rename(Date = Data, PLNEUR = Zamkniecie) %>%
+  mutate(Date = as.Date(Date, format = "%Y-%m-%d"))
+
+plneur_d <- read.csv("data/plneur_d.csv", sep = ",")[,c(1,5)] %>%
+  rename(Date = Data, PLNEUR = Zamkniecie) %>%
+  mutate(Date = as.Date(Date, format = "%Y-%m-%d"))
+
 daily_stocks <- dax_d %>% full_join(wig_d, by = "Date") %>%
-  rename(DAX = Close.x, Vol_GER = Volume.x, WIG = Close.y, Vol_PL = Volume.y) %>%
-  filter(across(everything(), complete.cases))
+  full_join(plneur_d, by = "Date") %>%
+  filter(across(everything(), complete.cases)) %>%
+  mutate(WIG = WIG*PLNEUR)
 
 monthly_stocks <- dax_m %>% full_join(wig_m, by = "Date") %>%
-  rename(DAX = Close.x, Vol_GER = Volume.x, WIG = Close.y, Vol_PL = Volume.y) %>%
-  filter(across(everything(), complete.cases))
+  full_join(plneur_m, by = "Date") %>%
+  filter(across(everything(), complete.cases)) %>%
+  mutate(WIG = WIG*PLNEUR)
 
 zew <- read.csv("data/ZEW_eurozone.csv", header = T, sep = ";", dec = ",")
 colnames(zew) <- c("Date", "EconomicGrowth", "Inflation", "CurrentSituation", "STOXX50", "InterestRate")
@@ -57,13 +67,6 @@ monthly_data <- full_join(monthly_stocks, zew, by = "Date") %>%
   dplyr::select(Date, DAX, WIG, Vol_GER, Vol_PL, EconomicGrowth, CurrentSituation,
                 Inflation, STOXX50, InterestRate) %>%
   mutate(DAX = log(DAX/lag(DAX)), WIG = log(WIG/lag(WIG)),
-         Vol_GER = (Vol_GER-lag(Vol_GER))/10000000, Vol_PL = (Vol_PL-lag(Vol_PL))/10000000) %>%
-  slice(-1) %>% filter(across(everything(), complete.cases))
-
-monthly_data <- full_join(monthly_stocks, zew, by = "Date") %>% 
-  dplyr::select(Date, DAX, WIG, Vol_GER, Vol_PL, EconomicGrowth, CurrentSituation,
-                Inflation, STOXX50, InterestRate) %>%
-  mutate(DAX = log(DAX/lag(DAX)), WIG = log(WIG/lag(WIG)),
          Vol_GER = (Vol_GER-lag(Vol_GER))/10000000, Vol_PL = (Vol_PL-lag(Vol_PL))/10000000,
          EG = EconomicGrowth-lag(EconomicGrowth),
          CS = CurrentSituation - lag(CurrentSituation),
@@ -79,27 +82,6 @@ adf.test(monthly_data$Vol_GER)
 adf.test(monthly_data$Vol_PL)
 adf.test(monthly_data$CurrentSituation)
 
-
-# PL
-VARselect(monthly_data[,-c(1,2,3,4)], lag.max = 15, type="const")
-model_PL <- VAR(monthly_data[,-c(1,2,3,4)], p = 2, type = "const")
-summary(model_PL)
-
-# GER
-VARselect(monthly_data[,-c(1,2,3,5)], lag.max = 15, type="const")
-model_GER <- VAR(monthly_data[,-c(1,2,3,5)], p = 2, type = "const")
-summary(model_GER)
-
-# both
-VARselect(monthly_data[,-c(1,2,3)], lag.max = 15, type="const")
-model <- VAR(monthly_data[,-c(1,2,3)], p = 2, type = "const")
-summary(model)
-
-
-feir <- irf(model_GER,
-            n.ahead = 5, ortho = T, runs = 1000)
-
-plot(feir)
 
 # VAR for sentiment indicators
 
@@ -152,13 +134,13 @@ monthly_data %>% dplyr::select(Date, EconomicGrowth, CurrentSituation, Inflation
 
 
 # check if volume impacts DAX with daily data
-VARselect(daily_data[,c(3,5)], lag.max = 30, type="const")
-model_d <- VAR(daily_data[,c(2:5)], p = 19, type = "const")
+VARselect(daily_data[,c(2:5)], lag.max = 30, type="const")
+model_d <- VAR(daily_data[,c(2:5)], p = 4, type = "const")
 
 summary(model)
 
-arch.test(model)
-serial.test(model)
+arch.test(model_d)
+serial.test(model_d)
 Box.test(residuals(model)[,3])
 causality(model, cause = "DAX")
 
